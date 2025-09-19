53260fe3e083160b3ba2d1c05cd5e45377f01840
somnath194 <somnathhalder194@gmail.com>
2025-09-19 22:08:14 +0530

    shifting to http

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.pem')),
+    port: 5173
     }
   }
-})
+)
